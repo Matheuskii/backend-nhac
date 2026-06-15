21f05ac1559b2b34cf89c1847c490333c8d4d62a
Matheus <alvesdemelomatheus4@gmail.com>
2026-06-14 23:58:35 -0300

    feat: initial commit with configured Spring Boot project

---
+++ src/main/java/br/com/nhac/backend_nhac/BackendNhacApplication.java
@@ -0,0 +1,13 @@
+package br.com.nhac.backend_nhac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendNhacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendNhacApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/nhac/backend_nhac/BackendNhacApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nhac.backend_nhac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendNhacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
