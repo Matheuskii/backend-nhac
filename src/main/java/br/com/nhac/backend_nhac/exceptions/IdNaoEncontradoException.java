40ff33950a42ca4b3a2a548c13ca9eafbb22d03e
Matheus <alvesdemelomatheus4@gmail.com>
2026-06-21 22:26:09 -0300

    feat(exceptions): implementar manipulador global de erros 404 e 500

---
+++ src/main/java/br/com/nhac/backend_nhac/exceptions/IdNaoEncontradoException.java
@@ -0,0 +1,7 @@
+package br.com.nhac.backend_nhac.exceptions;
+
+public class IdNaoEncontradoException extends RuntimeException {
+    public IdNaoEncontradoException(String message) {
+        super(message);
+    }
+}
