80b74d0cdf18f486798c7cc8747e11ea42a41cf5
Matheus <alvesdemelomatheus4@gmail.com>
2026-08-11 08:22:35 -0300

    add: adiciona testes de acordo com as mudanças de separação de endereco do usuario

--- src/test/java/br/com/nhac/backend_nhac/domain/usuario/UsuarioControllerTest.java
+++ src/test/java/br/com/nhac/backend_nhac/domain/usuario/UsuarioControllerTest.java
@@ -1,6 +1,7 @@
 package br.com.nhac.backend_nhac.domain.usuario;
 
 import br.com.nhac.backend_nhac.domain.usuario.dto.EnderecoUsuarioDTO;
+import br.com.nhac.backend_nhac.domain.usuario.dto.UsuarioAtualizarDTO;
 import br.com.nhac.backend_nhac.domain.usuario.dto.UsuarioCreateDTO;
 import br.com.nhac.backend_nhac.domain.usuario.dto.UsuarioResponseDTO;
 import br.com.nhac.backend_nhac.services.UsuarioService;
@@ -111,86 +112,15 @@ class UsuarioControllerTest {
     @Test
     @DisplayName("Deve retornar 204 ao atualizar dados parciais do próprio usuário")
     void deveAtualizarDadosDoProprioUsuario() throws Exception {
-        Map<String, Object> dados = Map.of("nome", "Nome Atualizado");
 
-        mockMvc.perform(patch("/api/v1/usuarios/{id}", USUARIO_LOGADO_ID)
+        UsuarioAtualizarDTO dados = new UsuarioAtualizarDTO("Nome Atualizado", null, null, null, null);
+
+        mockMvc.perform(put("/api/v1/usuarios/{id}", USUARIO_LOGADO_ID)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(dados)))
                 .andExpect(status().isNoContent());
 
-        verify(usuarioService, times(1)).atualizarUsuarioParcial(eq(USUARIO_LOGADO_ID), any());
+        verify(usuarioService, times(1)).atualizarUsuarioParcial(eq(USUARIO_LOGADO_ID), any(UsuarioAtualizarDTO.class));
     }
 
-    @Test
-    @DisplayName("Deve retornar 200 ao listar endereços do próprio usuário")
-    void deveListarEnderecosDoProprioUsuario() throws Exception {
-        EnderecoUsuarioDTO endereco = new EnderecoUsuarioDTO("end_1", "Rua A", "123", "Centro",
-                "SP", "SP", "01000-000", null, true);
-        when(usuarioService.listarEnderecos(USUARIO_LOGADO_ID)).thenReturn(List.of(endereco));
-
-        mockMvc.perform(get("/api/v1/usuarios/{usuarioId}/enderecos", USUARIO_LOGADO_ID))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$[0].id").value("end_1"));
-    }
-
-    @Test
-    @DisplayName("Deve retornar 201 ao adicionar um novo endereço válido")
-    void deveAdicionarEnderecoComSucesso() throws Exception {
-        EnderecoUsuarioDTO dto = new EnderecoUsuarioDTO(null, "Rua A", "123", "Centro",
-                "SP", "SP", "01000-000", null, true);
-
-        mockMvc.perform(post("/api/v1/usuarios/{usuarioId}/enderecos", USUARIO_LOGADO_ID)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(dto)))
-                .andExpect(status().isCreated());
-
-        verify(usuarioService, times(1)).adicionarEndereco(eq(USUARIO_LOGADO_ID), any());
-    }
-
-    @Test
-    @DisplayName("Deve retornar 400 ao adicionar endereço com CEP em formato inválido")
-    void deveRetornar422AoAdicionarEnderecoComCepInvalido() throws Exception {
-        EnderecoUsuarioDTO dto = new EnderecoUsuarioDTO(null, "Rua A", "123", "Centro",
-                "SP", "SP", "cep-invalido", null, true);
-
-        //noinspection deprecation
-        mockMvc.perform(post("/api/v1/usuarios/{usuarioId}/enderecos", USUARIO_LOGADO_ID)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(dto)))
-                .andExpect(status().isBadRequest());
-
-        verify(usuarioService, never()).adicionarEndereco(any(), any());
-    }
-
-    @Test
-    @DisplayName("Deve retornar 204 ao atualizar um endereço do próprio usuário")
-    void deveAtualizarEnderecoComSucesso() throws Exception {
-        EnderecoUsuarioDTO dto = new EnderecoUsuarioDTO("end_1", "Rua Nova", "456", "Centro",
-                "SP", "SP", "01000-000", null, true);
-
-        mockMvc.perform(put("/api/v1/usuarios/{usuarioId}/enderecos/{enderecoId}", USUARIO_LOGADO_ID, "end_1")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(dto)))
-                .andExpect(status().isNoContent());
-
-        verify(usuarioService, times(1)).atualizarEndereco(eq(USUARIO_LOGADO_ID), eq("end_1"), any());
-    }
-
-    @Test
-    @DisplayName("Deve retornar 204 ao remover um endereço do próprio usuário")
-    void deveRemoverEnderecoComSucesso() throws Exception {
-        mockMvc.perform(delete("/api/v1/usuarios/{usuarioId}/enderecos/{enderecoId}", USUARIO_LOGADO_ID, "end_1"))
-                .andExpect(status().isNoContent());
-
-        verify(usuarioService, times(1)).removerEndereco(USUARIO_LOGADO_ID, "end_1");
-    }
-
-    @Test
-    @DisplayName("Deve retornar 403 ao tentar remover endereço de outro usuário")
-    void deveRetornar403AoRemoverEnderecoDeOutroUsuario() throws Exception {
-        mockMvc.perform(delete("/api/v1/usuarios/{usuarioId}/enderecos/{enderecoId}", "outro_usuario_id", "end_1"))
-                .andExpect(status().isForbidden());
-
-        verify(usuarioService, never()).removerEndereco(any(), any());
-    }
 }
