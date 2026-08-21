60bdcc53f89626d7d8bbe8c4cb9056b5a322fc6e
Matheus <alvesdemelomatheus4@gmail.com>
2026-08-21 15:28:18 -0300

    fix(pagamento): corrige testes e configura bean do RestTemplate
    Resolve NullPointerException nos testes inicializando o Gson diretamente, adiciona mock do PedidoRepository faltante e declara o Bean do RestTemplate na classe principal para o contexto do Spring.

--- src/main/java/br/com/nhac/backend_nhac/BackendNhacApplication.java
+++ src/main/java/br/com/nhac/backend_nhac/BackendNhacApplication.java
@@ -10,4 +10,9 @@ public class BackendNhacApplication {
 		SpringApplication.run(BackendNhacApplication.class, args);
 	}
 
+	@org.springframework.context.annotation.Bean
+	public org.springframework.web.client.RestTemplate restTemplate() {
+		return new org.springframework.web.client.RestTemplate();
+	}
+
 }
