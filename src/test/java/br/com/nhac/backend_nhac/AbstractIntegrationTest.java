2079b51d20d280ad14091c565a7e52a7d0a691d0
MATHEUS ALVES DE MELO <25163072@edu119.local>
2026-09-14 16:47:39 -0300

    test: isolate integration fixtures with ordered database cleanup

--- src/test/java/br/com/nhac/backend_nhac/AbstractIntegrationTest.java
+++ src/test/java/br/com/nhac/backend_nhac/AbstractIntegrationTest.java
@@ -5,12 +5,15 @@ import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
+@Sql(scripts = "/cleanup.sql", config = @SqlConfig(transactionMode = SqlConfig.TransactionMode.ISOLATED))
 public abstract class AbstractIntegrationTest {
 
     protected MockMvc mockMvc;
